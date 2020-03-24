00fa325a80e7c83948b583c37557c4ecb02dcd02
Abdul Mardini <superppl@gmail.com>
2020-03-23 20:43:20 -0700

    switched parser to strtok

--- memeshell.c
+++ memeshell.c
@@ -19,18 +19,28 @@
 #define ARG_MAX      8
 #define ARG_BUF      64
 
+#define TOKEN_DELIM " \t\r\n\a"
+
 #define CONTINUE     1
 #define STOP         0
 
+/* Stealing control+c so you can't exit the program that way. */
 void sigint_handler(int signum);
+/* Prints the shell stuff. Eventually going to do more. */
 void print_shell(void);
+/* Reads the input a line at a time. */
 char *read_line(void);
+/* Parses the line to get the commands and the arguments. */
 char **read_args(char *line);
+/* Checks if we've reach a character I should treat as a delimiter. */
 int line_delim(char c);
+/* Takes the parsed line, and runs the command it got from it. */
 int run_cmd(char **cmd);
+/* Built in pwd tool. */
 int pwd();
+/* chdir to change the directory. */
 int cd(char **cmd);
-int exit_shell(char **cmd);
+/* Uses fork and execvp to call an external tool. */
 int external(char **cmd);
 
 int main(void) {
@@ -45,17 +55,18 @@ int main(void) {
 		status = run_cmd(cmd);
 
 		free(line);
-		for (int i = 0; i < ARG_MAX; i++)
-			free(cmd[i]);
+		//for (int i = 0; cmd[i] != NULL; i++)
+		//	free(cmd[i]);
 		free(cmd);
 	}
+	printf("\n");
 	return EXIT_SUCCESS;
 }
 
 void print_shell() {
 		/* Print prompt */
-		printf("sshell$ ");
-			fflush(stdout);
+		printf("memeshell> ");
+		fflush(stdout);
 }
 
 char *read_line(void){
@@ -65,7 +76,8 @@ char *read_line(void){
 		exit(EXIT_FAILURE);
 	}
 	memset(line, 0, LINE_BUFFER);
-	fgets(line, LINE_BUFFER, stdin);
+	if (fgets(line, LINE_BUFFER, stdin) == NULL)
+		exit(EXIT_SUCCESS);
 	return line;
 }
 
@@ -77,52 +89,77 @@ int line_delim(char c){
 
 char **read_args(char *line){
 
-	/* Initialize and clear the argument buffer. */
-	char **cmd = malloc(sizeof(char*) * ARG_MAX);
+	// /* Initialize and clear the argument buffer. */
+	// char **cmd = malloc(sizeof(char*) * ARG_MAX);
+	// if (cmd == NULL) {
+	// 	fprintf(stderr, "Your system has some issues.\n");
+	// 	exit(EXIT_FAILURE);
+	// }
+
+	// // for (int i = 0; i < ARG_MAX; i++) {
+	// // 	cmd[i] = malloc(sizeof(char) * ARG_BUF);
+	// // 	if (cmd[i] == NULL) {
+	// // 		fprintf(stderr, "Your system has some issues.\n");
+	// // 		exit(EXIT_FAILURE);
+	// // 	}
+	// // 	memset(cmd[i], 0, ARG_BUF);
+	// // }
+
+	// if (line[0] == '\n') {
+	// 	cmd[0][0] = '\n';
+	// 	return cmd;
+	// }
+
+	// int i = 0, j = 0, k = 0;
+
+	// /* Trim any whitespace in the beginning. */
+	// while (line_delim(line[i])) {
+	// 	i++;
+	// }
+
+	// for (; i < LINE_BUFFER, j < ARG_MAX; i++) {
+	// 	if (cmd[j] == NULL) {
+	// 		cmd[j] = malloc(sizeof(char) * ARG_BUF);
+	// 		if (cmd[j] == NULL) {
+	// 			fprintf(stderr, "Your system has some issues.\n");
+	// 			exit(EXIT_FAILURE);
+	// 		}
+	// 		memset(cmd[j], 0, ARG_BUF);
+	// 	}
+	// 	if (line_delim(line[i])) {
+	// 		/* Null terminate the string. */
+	// 		//cmd[j][k] = '\0';
+	// 		j++;
+	// 		k = 0;
+	// 	} else {
+	// 		cmd[j][k] = line[i];
+	// 		k++;
+	// 	}
+	// }
+	char **cmd = malloc(sizeof(char*) * ARG_MAX), *token;
+	int i;
 	if (cmd == NULL) {
 		fprintf(stderr, "Your system has some issues.\n");
 		exit(EXIT_FAILURE);
 	}
 
-	for (int i = 0; i < ARG_MAX; i++) {
-		cmd[i] = malloc(sizeof(char) * ARG_BUF);
-		if (cmd[i] == NULL) {
-			fprintf(stderr, "Your system has some issues.\n");
-			exit(EXIT_FAILURE);
-		}
-		memset(cmd[i], 0, ARG_BUF);
+	token = strtok(line, TOKEN_DELIM);
+
+	for (i = 0; i < ARG_MAX - 1 || token != NULL; i++) {
+		cmd[i] = token;
+		token = strtok(NULL, TOKEN_DELIM);
 	}
 
-	if (line[0] == '\n') {
-		cmd[0][0] = '\n';
-		return cmd;
-	}
-
-	int i = 0, j = 0, k = 0;
-
-	/* Trim any whitespace in the beginning. */
-	while (line_delim(line[i])) {
-		i++;
-	}
-
-	for (; i < LINE_BUFFER; i++) {
-		if (line_delim(line[i])) {
-			j++;
-			k = 0;
-		} else {
-			cmd[j][k] = line[i];
-			k++;
-		}
-	}
+	cmd[++i] = NULL;
 
 	return cmd;
 }
 
 int run_cmd(char **cmd) {
-	if (exit_shell(cmd)) {
-		return STOP;
-	} else if (cmd[0][0] == '\n') {
+	if (cmd[0]== NULL) {
 		return CONTINUE;
+	} else if (strcmp(cmd[0], "exit") == 0) {
+		return STOP;
 	} else if (strcmp(cmd[0], "cd")  == 0) {
 		return cd(cmd);
 	} else if (strcmp(cmd[0], "pwd")  == 0) {
@@ -157,10 +194,6 @@ void sigint_handler(int signum) {
 	printf("BWA HA HA HA HA HA HA\n");
 }
 
-int exit_shell(char **cmd) {
-	return (strcmp(cmd[0], "exit") == 0 || strcmp(cmd[0], "\0") == 0 );
-}
-
 int external(char **cmd) {
 	pid_t pid;
 	int status;
@@ -175,6 +208,7 @@ int external(char **cmd) {
 		exit(EXIT_FAILURE);
 	} else {
 		waitpid(-1, &status, 0);
-		return WEXITSTATUS(status);
+		fprintf(stderr,"%c\n", WEXITSTATUS(status));
+		return CONTINUE;
 	}
 }
